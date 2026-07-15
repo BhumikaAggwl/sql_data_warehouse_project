cb0a45d69e8a3b789974db9dfad501b6719e1a9a
Bhumika <bhumikaaggarwal576@gmail.com>
2026-07-16 00:47:43 +0530

    Create crm_cust_info_silver_load.sql

---
+++ scripts/silver/load_into_silver/crm_cust_info_silver_load.sql
@@ -0,0 +1,30 @@
+INSERT INTO silver.crm_cust_info(
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date)
+
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) as cst_firstname,
+TRIM(cst_lastname)  as cst_lastname ,
+CASE WHEN UPPER(TRIM(cst_marital_status))='M' THEN 'MARRIED'
+WHEN UPPER(TRIM(cst_marital_status)) ='S' THEN 'SINGLE'
+ELSE 'UNKNOWN'
+END cst_marital_status ,
+CASE WHEN UPPER(TRIM(cst_gndr))='F' THEN 'FEMALE' 
+WHEN UPPER(TRIM(cst_gndr))='M' THEN 'MALE'
+ELSE 'UNKNOWN'
+END cst_gndr,
+cst_create_date
+FROM(
+	SELECT 
+	*,
+	ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+	FROM bronze.crm_cust_info 
+	WHERE cst_id IS NOT NULL 
+)t WHERE flag_last=1
